45aa429a1efa413b89aef47fefe7eb071b3640bf
dina82 <dinaabdellatif94@gmail.com>
2020-06-26 14:16:54 +0300

    update section of our team

--- js/script.js
+++ js/script.js
@@ -19,5 +19,47 @@ $(document).ready(function () {
     loop:true,
     autoplay:true
 });
-  
+  // magnific popper
+
+// $('#manager-message').magnificPopup({
+//   items: [
+//     {
+//       src: './images/services/service2.jpg',
+//       title: 'Peter & Paul fortress in SPB'
+//     },
+//     {
+//       src: './images/services/service5.jpg',
+//       type: 'iframe' // this overrides default type
+//     },
+//     {
+//       src: $('<div class="white-popup">Dynamically created element</div>'), // Dynamically created element
+//       type: 'inline'
+//     },
+//     {
+//       src: '<div class="white-popup">Popup from HTML string</div>', // HTML string
+//       type: 'inline'
+//     }
+//   ],
+//   gallery: {
+//     enabled: true
+//   },
+//   type: 'image' // this is a default type
+// });
+$('#open-popup').magnificPopup({
+  items: [
+    {
+      src: './images/services/service2.jpg',
+      title: 'Peter & Paul fortress in SPB'
+    },
+    {
+      src: '<div class="white-popup">Dynamically created popup</div>',
+      type: 'inline'
+  }
+    
+  ],
+  gallery: {
+    enabled: true
+  },
+  type: 'image'
+});
 })
